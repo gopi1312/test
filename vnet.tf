1d486df028714c9af735f5d9a256936adaf89dbf
gopi1312 <gopinath.siva1312@gmail.com>
2021-07-05 12:39:12 +0530

    first commit

---
+++ vnet.tf
@@ -0,0 +1,20 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+provider "azurerm" {
+version = "=2.0"
+features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "vnet-test-rg"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group.
+resource "azurerm_virtual_network" "example" {
+  name                = "virtualnetwork-test01"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
